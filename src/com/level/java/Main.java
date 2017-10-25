0eef2833df6fe232d62465c600663e1b88e0406e
alduin <voodoonv82@gmail.com>
2017-10-26 01:19:16 +0300

    two java-file

---
+++ src/com/level/java/Main.java
@@ -0,0 +1,7 @@
+package com.level.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git");
+    }
+}
---
+++ src/com/level/java/Main2.java
@@ -0,0 +1,7 @@
+package com.level.java;
+
+public class Main2 {
+    public static void main(String [] args){
+        System.out.println("Hello Git2");
+    }
+}
